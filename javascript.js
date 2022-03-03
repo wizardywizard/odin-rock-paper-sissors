a74b666dd2ca419df5817922c93f9f00e0a5e850
wizardywizard <littledavid175@gmail.com>
2022-03-02 18:13:31 -0800

    first commit add html file and js file

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let choice = ["Rock", "Paper", "Sissors"]
+
+function computerPlay () {
+    
+    return choice[Math.floor(Math.random()*choice.length)];
+}
+
+function playRound(playerChoice, computerChoice) {
+
+}
