d034fa698272975b3e7f50ddef5c3cf430323366
wizardywizard <littledavid175@gmail.com>
2022-03-06 12:36:31 -0800

    made changes to play round

--- javascript.js
+++ javascript.js
@@ -21,21 +21,25 @@ function playRound(playerChoice, computerChoice) {
     ) {
         playerScore++
         winner = "player"
+        return console.log(winner)
     } else if ( 
         (computerChoice == "rock" && playerChoice == "sissors") ||
         (computerChoice == "sissors" && playerChoice == "papper") ||
         (computerChoice == "papper" && playerChoice == "rock")
     ) {
         computerScore++
-        winner = "computer"   
+        winner = "computer" 
+        return console.log(winner)  
     }
 }
 
 function game() {
-    if (computerScore == 5){
-        return alert("computer wins")
+    if (computerScore != 5 || playerScore != 5){
+        playRound(playerChoice, computerChoice)
     } else if (playerScore == 5){
         return alert("player wins")
+    } else if (computerScore == 5){
+        return alert("computer wins")
     }
 }
 
