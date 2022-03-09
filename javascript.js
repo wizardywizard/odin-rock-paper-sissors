19eb22d22fa3872c405a390119d3a31eb6fecc38
wizardywizard <littledavid175@gmail.com>
2022-03-08 18:52:26 -0800

    worked on javascript file

--- javascript.js
+++ javascript.js
@@ -1,45 +1,94 @@
-let choice = ["rock", "paper", "sissors"]
-let playerScore = 0
-let computerScore = 0
-let winner = ""
+let playerScore = 0;
+let computerScore = 0;
+const playerScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const sissors_div = document.getElementById("s");
 
-function computerPlay () {
-    
-    return choice[Math.floor(Math.random()*choice.length)];
+function getComputerChoice(){
+    const choices =["r", "p", "s"];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
 }
 
-function playRound(playerChoice, computerChoice) {
+function convertToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "paper";
+    return "Sissors";
+}
 
-    if(computerChoice == playerChoice) {
-        winner = "tie"
-    } 
-    
-    if ( 
-        (playerChoice == "rock" && computerChoice =="sissors") ||
-        (playerChoice == "sissors" && computerChoice == "papper") ||
-        (playerChoice == "papper" && computerChoice == "rock")
-    ) {
-        playerScore++
-        winner = "player"
-        return console.log(winner)
-    } else if ( 
-        (computerChoice == "rock" && playerChoice == "sissors") ||
-        (computerChoice == "sissors" && playerChoice == "papper") ||
-        (computerChoice == "papper" && playerChoice == "rock")
-    ) {
-        computerScore++
-        winner = "computer" 
-        return console.log(winner)  
+function win(userChoice, computerChoice){
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    result_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(computerChoice)}. you win!`;
+}
+
+function lose(){
+    console.log("lose");
+}
+
+function draw(){
+    console.log("it's a draw");
+}
+
+function game(userChoice){
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
     }
 }
 
-function game() {
-    if (computerScore != 5 || playerScore != 5){
-        playRound(playerChoice, computerChoice)
-    } else if (playerScore == 5){
-        return alert("player wins")
-    } else if (computerScore == 5){
-        return alert("computer wins")
-    }
+function main(){
+    rock_div.addEventListener("click", function(){
+        game("r");
+    })
+
+    paper_div.addEventListener("click", function(){
+        game("p");
+    })
+
+    sissors_div.addEventListener("click", function(){
+        game("s");
+    })
 }
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
